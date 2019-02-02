a7552fa29f67161265c6a09def130afe226666a2
rainbow85213 <rainbow852@matam.co.kr>
2019-02-03 02:43:52 +0900

    first

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rainbow85213
+ * Date: 2019-02-03
+ * Time: 02:39
+ */
+
+namespace Rainbow85213\Packagetest;
+
+
+class Hello
+{
+	public function world()
+	{
+		return "Hello Heedong!!!";
+	}
+}
